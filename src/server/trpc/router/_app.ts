182d190267daaae8805cb96352604edad8f6fa80
Kyle Rassweiler <dev@kylerassweiler.ca>
2022-12-29 21:52:46 -0500

    add routers and remove examples
    Signed-off-by: Kyle Rassweiler <dev@kylerassweiler.ca>

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,13 @@
 import { router } from '../trpc';
 import { authRouter } from './auth';
 import { boardRouter } from './board';
+import { cardRouter } from './card';
+import { listRouter } from './list';
 
 export const appRouter = router({
 	board: boardRouter,
+	list: listRouter,
+	card: cardRouter,
 	auth: authRouter,
 });
 
